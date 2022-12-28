bd90e9266215c16e0f6688997c317a5030c9333f
Ahmed Ali <ahmedali5696@outlook.com>
2022-12-28 20:42:02 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
-import '../styles/globals.css'
-import { Tajawal } from '@next/font/google'
-import Layout from '../components/Layout'
 import { useEffect, useState } from 'react'
+import { Tajawal } from '@next/font/google'
 
-const tajawal = Tajawal({
-  weight: ['400', '500', '700', '800', '900'],
-  subsets: ['latin'],
-})
+import Layout from '../components/Layout'
+import '../styles/globals.css'
+
+
+// const tajawal = Tajawal({
+//   weight: ['400', '500', '700', '800', '900'],
+//   subsets: ['latin'],
+// })
 
 
 export default function App({ Component, pageProps }) {
@@ -27,11 +29,11 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <style jsx global>{`
-        body {
-          font-family: ${tajawal.style.fontFamily};
-        }
-      `}</style>
+//       <style jsx global>{`
+//         body {
+//           font-family: ${tajawal.style.fontFamily};
+//         }
+//       `}</style>
       <Layout>
         <Component {...pageProps} />
       </Layout>
